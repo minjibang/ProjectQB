5224078d2fb001897b14391a994a8297fd3c6b73
HanGyul <whg0401@gmail.com>
2018-10-15 14:15:14 +0900

    한결  :  강사 시험지 만들기 데이터 뿌리기 - 컨트롤러, 서비스, 다오, mapper, vo 설정 >> vo 일부분은 강제로 만들어줌

--- QB_UI/src/main/java/onet/com/teacher/service/TeacherService.java
+++ QB_UI/src/main/java/onet/com/teacher/service/TeacherService.java
@@ -1,9 +1,30 @@
 package onet.com.teacher.service;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import onet.com.teacher.dao.TeacherDao;
+import onet.com.vo.QuestionDto;
+import onet.com.vo.Question_choiceDto;
 
 @Service
 public class TeacherService {
 	
+	@Autowired
+	private SqlSession sqlsession;
+	
+	public List<QuestionDto> question(){
+		TeacherDao dao = sqlsession.getMapper(TeacherDao.class);
+		List<QuestionDto> result = dao.question();
+		return result;
+	}
+	public List<Question_choiceDto> question_choice(){
+		TeacherDao dao = sqlsession.getMapper(TeacherDao.class);
+		List<Question_choiceDto> result = dao.question_choice();
+		return result;
+	}
 	
 }
