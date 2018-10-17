2ace3915b74d2f00ea3c0dbc098c7021dc0e0d13
DESKTOP-R4RDOSB\bituser <kim5518200@naver.com>
2018-10-17 18:01:28 +0900

    현이 - QuestionDto, Question_choiceDto 자료형 수정

--- QB_UI/src/main/java/onet/com/vo/QuestionDto.java
+++ QB_UI/src/main/java/onet/com/vo/QuestionDto.java
@@ -2,35 +2,21 @@ package onet.com.vo;
 
 public class QuestionDto {
 
-	private String question_num;
+	private int question_num;
 	private String question_type;
 	private String question_name;
 	private String question_img;
 	private String question_answer;
-	private String question_correct_ratio;
-	private String question_select_count;
+	private int question_correct_ratio;
+	private int question_select_count;
 	private String sm_category_code;
 	private String level_code;
 	private String member_id;
 	
-	/* 문제 뿌려주기 시 코드 아닌 이름 불러오기 */
-	private String sm_category_name;
-	private String md_category_name;
-	private String lg_category_name;
-	private String level_name;
-	
-	
-	
-	public String getLg_category_name() {
-		return lg_category_name;
-	}
-	public void setLg_category_name(String lg_category_name) {
-		this.lg_category_name = lg_category_name;
-	}
-	public String getQuestion_num() {
+	public int getQuestion_num() {
 		return question_num;
 	}
-	public void setQuestion_num(String question_num) {
+	public void setQuestion_num(int question_num) {
 		this.question_num = question_num;
 	}
 	public String getQuestion_type() {
@@ -57,16 +43,16 @@ public class QuestionDto {
 	public void setQuestion_answer(String question_answer) {
 		this.question_answer = question_answer;
 	}
-	public String getQuestion_correct_ratio() {
+	public int getQuestion_correct_ratio() {
 		return question_correct_ratio;
 	}
-	public void setQuestion_correct_ratio(String question_correct_ratio) {
+	public void setQuestion_correct_ratio(int question_correct_ratio) {
 		this.question_correct_ratio = question_correct_ratio;
 	}
-	public String getQuestion_select_count() {
+	public int getQuestion_select_count() {
 		return question_select_count;
 	}
-	public void setQuestion_select_count(String question_select_count) {
+	public void setQuestion_select_count(int question_select_count) {
 		this.question_select_count = question_select_count;
 	}
 	public String getSm_category_code() {
@@ -89,22 +75,11 @@ public class QuestionDto {
 	}
 	
 	/* 문제 뿌려주기 시 코드 아닌 이름 불러오기 */
-	public String getSm_category_name() {
-		return sm_category_name;
-	}
-	public void setSm_category_name(String sm_category_name) {
-		this.sm_category_name = sm_category_name;
-	}
-	public String getMd_category_name() {
-		return md_category_name;
-	}
-	public void setMd_category_name(String md_category_name) {
-		this.md_category_name = md_category_name;
-	}
-	public String getLevel_name() {
-		return level_name;
-	}
-	public void setLevel_name(String level_name) {
-		this.level_name = level_name;
-	}
+/*	private String sm_category_name;
+	private String md_category_name;
+	private String lg_category_name;
+	private String level_name;*/
+	
+	
+	
 }
--- QB_UI/src/main/java/onet/com/vo/Question_choiceDto.java
+++ QB_UI/src/main/java/onet/com/vo/Question_choiceDto.java
@@ -2,20 +2,20 @@ package onet.com.vo;
 
 public class Question_choiceDto {
 
-	private String question_num;
-	private String question_choice_num;
+	private int question_num;
+	private int question_choice_num;
 	private String question_choice_content;
 	
-	public String getQuestion_num() {
+	public int getQuestion_num() {
 		return question_num;
 	}
-	public void setQuestion_num(String question_num) {
+	public void setQuestion_num(int question_num) {
 		this.question_num = question_num;
 	}
-	public String getQuestion_choice_num() {
+	public int getQuestion_choice_num() {
 		return question_choice_num;
 	}
-	public void setQuestion_choice_num(String question_choice_num) {
+	public void setQuestion_choice_num(int question_choice_num) {
 		this.question_choice_num = question_choice_num;
 	}
 	public String getQuestion_choice_content() {
@@ -25,4 +25,6 @@ public class Question_choiceDto {
 		this.question_choice_content = question_choice_content;
 	}
 	
+	
+
 }
