9f19921d99be1bad3c6be6c65f1ea1dd9545839e
YangHoeJun <steepjun@gmail.com>
2018-10-18 11:21:05 +0900

    양회준 - 문제 만들기 보기 입력 해결

--- QB_UI/src/main/java/onet/com/admin/dao/AdminDao.java
+++ QB_UI/src/main/java/onet/com/admin/dao/AdminDao.java
@@ -24,8 +24,7 @@ public interface AdminDao {
 	
 	/*  재훈 - 10.16 새 문제 만들기 관련 시작 */
 	public int insertQuestion(QuestionDto dto);
-	public int insertQuestionChoice(String string, Map<String, Object> map);
-	/*public int insertQuestionChoice(Question_choiceDto dto);*/
+	public int insertQuestionChoice(int question_num, String question_choice_num, String question_choice_content);
 	/*  재훈 - 10.16 새 문제 만들기 관련 끝 */
 	
 	/* 영준 - 10.10 회원관리 관련 시작 */
