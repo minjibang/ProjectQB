3db1676fac107a8a5bf8ee549475dce502c3235e
HanGyul <whg0401@gmail.com>
2018-10-25 19:02:17 +0900

    한결 시험지 만들기 이미지 조정

--- QB_UI/src/main/java/onet/com/teacher/controller/TeacherController.java
+++ QB_UI/src/main/java/onet/com/teacher/controller/TeacherController.java
@@ -246,13 +246,10 @@ public class TeacherController {
 		@RequestMapping(value="insertQuestion.do", method=RequestMethod.POST)
 		public String insertQuestion(QuestionDto dto2, Question_choiceDto dto, HttpServletRequest request)
 			throws IOException, ClassNotFoundException, SQLException{
-			System.out.println("컨트롤러 진입>>");
-			System.out.println(dto2.getQuestion_img() + "\n" + dto2.getQuestion_file() + "\n" + dto);
 			if (dto2.getQuestion_type().equals("객관식")) {
 				adminService.insertQuestion(dto2, request);
 				adminService.insertQuestionChoice(dto2, dto, request);
 			} else if(dto2.getQuestion_type().equals("단답형")) {
-				System.out.println("컨트롤러 단답형 엘스 진입>>");
 				adminService.insertQuestion(dto2, request);
 			}
 			return "redirect:questionManagement.do";
@@ -437,7 +434,7 @@ public class TeacherController {
 		String originFileName2 = file2.getOriginalFilename();
 		long fileSize1 = file1.getSize();
 		long fileSize2 = file2.getSize();
-		String path =  request.getServletContext().getRealPath("resources/upload/board/");
+		String path =  request.getServletContext().getRealPath("/upload/notice/");
 		
 		UUID uuid = UUID.randomUUID();
 		String savaFile1 = uuid.toString()+"_" + originFileName1;
